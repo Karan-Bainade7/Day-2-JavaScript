325b34934cdda6e6c4ffc046bb05d5e479a76bde
karan punamsing bainade <119426472+Karan-Bainade7@users.noreply.github.com>
2024-11-23 22:42:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let a = 18;
+if (a > 17) {
+    console.log("you are  not enligeble to vote");
+}
+let day = "sunday";
+if (day == "monday") {
+    console.log("Good day");
+} else {
+    console.log("Good bad day");
+
+}
+
+if condition
+
+let Day = 7;
+let week = 1;
+if (Day < week) {
+    console.log("welcome to you");
+} else if (Day == week) {
+    console.log("no");
+
+} else {
+    console.log("yes");
+}
